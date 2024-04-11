0b60ba69aaa93bf680c87fd7a8bec4170d9e0be1
masterbatcoderman10 <suhaib.athar@gmail.com>
2024-04-12 02:24:52 +0400

    added new endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.post("/")
 def read_root():
     return {"action": "reply", "replies": ["Hello, World!"]}
+
+@app.post("/echo")
+def read_item():
+    return {"action": "reply", "replies": ["Echo!"]}
